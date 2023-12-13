267d873f3560aa2099f8b1680e35053df651b052
Otabek Rustamov <otabek.pro@hotmail.com>
2023-12-13 18:30:09 +0500

    test v2

--- GIReporter/Commands/MyProjectsCommand.cs
+++ GIReporter/Commands/MyProjectsCommand.cs
@@ -46,7 +46,7 @@ namespace GIReporter.Commands
                     var info = chat.Title ?? chat.FirstName ?? chat.Username;
                     var isChatMemberBot = await _botClient.GetChatMemberAsync(p.ChatId, botId);
                     await Console.Out.WriteLineAsync(isChatMemberBot.Status.ToString());
-                    projectStrings.AppendLine($"({info}) `{p.ChatId.ToString()}:{p.Name}`");
+                    projectStrings.AppendLine($"```({info}) {p.ChatId.ToString()}:{p.Name}```");
 
                     await Console.Out.WriteLineAsync(projectStrings);
                 }
